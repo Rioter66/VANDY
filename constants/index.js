aa1cf2757498ed488430117446841c53ab13fe23
Farnood <90553874+Rioter66@users.noreply.github.com>
2023-07-27 08:02:04 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xc44862f56793642258cbe7ded35c444ec72f3d07";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
